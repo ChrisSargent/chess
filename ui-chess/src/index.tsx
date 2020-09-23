8afcd18fb76803e39183e2a8605125ffd39511b7
Chris Sargent <chris@stickypixel.com>
2020-09-23 10:50:48 +0200

    update to use string injection from fmp

--- ui-chess/src/index.tsx
+++ ui-chess/src/index.tsx
@@ -1,39 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Chessboard from "chessboardjsx";
 import "reset-css";
 import "./index.css";
 
-declare global {
-  interface Window {
-    setBoardPosition: any;
-  }
-}
-
-const Chess = () => {
-  const { search } = window.location;
-  const params = new URLSearchParams(search);
-  const positionFromURL = params.get("position");
-
-  const [position, setPosition] = useState(positionFromURL || "__BOARD_POSITION__");
-  window.setBoardPosition = setPosition;
-
-  return (
-    <Chessboard
-      calcWidth={({ screenWidth, screenHeight }) => {
-        console.log(screenWidth);
-
-        return Math.min(screenWidth, screenHeight);
-      }}
-      draggable={false}
-      position={position}
-    />
-  );
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <Chess />
+    <Chessboard
+      calcWidth={({ screenWidth, screenHeight }) => Math.min(screenWidth, screenHeight)}
+      draggable={false}
+      position="__BOARD_POSITION__"
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
